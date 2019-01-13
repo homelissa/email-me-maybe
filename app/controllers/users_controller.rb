6141be15197f438a9dca3bfa3c314c788e9ad71d
Melissa Ho <homelissa93@gmail.com>
2019-01-13 09:12:06 -0800

    Mailgun API via mailgun-ruby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+
+        ExampleMailer.sample_email(@user).deliver
+        
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
